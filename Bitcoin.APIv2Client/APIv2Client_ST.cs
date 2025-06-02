65ba49c2a98288093c9a7d61377777441e8242df
Tomas Prokop <xprokopt@gmail.com>
2025-06-02 12:14:59 +0200

    Initial commit

---
+++ Bitcoin.APIv2Client/APIv2Client_ST.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoin.APIv2Client
+{
+    public static class APIv2Client_ST
+    {
+        private static APIv2Client _apIv2Client;
+
+        static APIv2Client_ST()
+        {
+            _apIv2Client = new APIv2Client();
+        }
+
+        public static APIv2Client Tools{
+            get { return _apIv2Client; }
+            set { value = _apIv2Client; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# APIv2Client - [obsolete] 2017
